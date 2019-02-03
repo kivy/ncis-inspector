a62edf427292aadc7b180b392729733e3e23afb7
Gabriel Pettier <gabriel.pettier@gmail.com>
2019-02-03 19:01:54 +0100

    make the app save the config, with split_ratios in it

--- inspector/standalone.py
+++ inspector/standalone.py
@@ -1,5 +1,9 @@
+from os.path import exists, dirname
+from os import makedirs
+
 from kaki.app import App
 from kivy.factory import Factory as F
+from kivy.properties import StringProperty
 from inspector.controller import discover_classes
 
 
@@ -14,8 +18,35 @@ class Application(App):
         ('inspector', {'recursive': True}),
     ]
 
+    name = StringProperty("NCIS-Dash")
+
     def build_app(self):
+        self.load_config()
         return F.InspectorApplicationRoot()
+
+    def load_config(self):
+        config = super(Application, self).load_config()
+        if not config.filename:
+            config.filename = self.get_application_config()
+
+    def build_config(self, config):
+        config.setdefaults('general', {
+            'version': '0'
+        })
+
+    def get_application_config(self):
+        if exists('{}.ini'.format(self.name)):
+            path = '{}.ini'.format(self.name)
+        else:
+            path = '{}/%(appname)s.ini'.format(
+                self.user_data_dir
+            )
+
+        cfg = super(Application, self).get_application_config(path)
+        d = dirname(cfg)
+        if d and not exists(d):
+            makedirs(d)
+        return cfg
 
 
 if __name__ == '__main__':
